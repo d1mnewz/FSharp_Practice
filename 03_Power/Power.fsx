6e4707a5cae89d71dd624554c365c50a156cbe84
ZhluktenkoDmytro <d1mnewz@gmail.com>
2017-01-27 00:18:08 +0200

    code refactoring for "03_Power"

--- 03_Power/Power.fsx
+++ 03_Power/Power.fsx
@@ -6,6 +6,12 @@ let main() =
     //let a = ReadInput float // |> - what type of result we want from console.readline()
     //let b = ReadInput float // actually this is a explicit cast operator like (int)a
     Console.WriteLine(Math.Pow(ReadInput float, ReadInput float))
+
+    // OR
+     
+    //let a = ReadInput float // |> - what type of result we want from console.readline()
+    //let b = ReadInput float // actually this is a explicit cast operator like (int)a
+    //Console.WriteLine("{0}^{1} = {2}", a, b, Math.Pow(a, b).ToString())
 main() // started wrtiting in vs code
 
 // refactored so now it looks like functional programming
