0f4d4cf04a979ff8a628a7d80d4d41fcaa1964c2
ZhluktenkoDmytro <d1mnewz@gmail.com>
2017-03-23 21:11:36 +0200

    add example of immutable and mutable data

---
+++ 14_ImmutableData/im_mutable.fsx
@@ -0,0 +1,20 @@
+// thats very bad code down here!
+
+// let  msg = "Dima is awesome!";
+// printfn "%s" msg;
+// msg = "Dima is super awesome!";
+// printfn "%s" msg;
+
+// well, this one seem to be better, but it's still so imperitive!
+
+// let mutable msg = "Dima is awesome!";
+// printfn "%s" msg;
+// msg <- "Dima is super awesome!";
+// printfn "%s" msg;
+
+// that's how you do this like a real fella!
+
+[   "Dima is awesome"
+    "Dima is super awesome!"
+    "Alex is cool!"
+] |> Seq.iter (printfn "%s")
