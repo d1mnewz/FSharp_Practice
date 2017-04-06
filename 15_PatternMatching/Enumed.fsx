bac23fd67b9c71d3edf2e7231dcf73dc74b50c6e
ZhluktenkoDmytro <d1mnewz@gmail.com>
2017-04-06 23:53:01 +0200

    enum pattern matching fix

--- 15_PatternMatching/Enumed.fsx
+++ 15_PatternMatching/Enumed.fsx
@@ -1,64 +1,30 @@
 
 // records
 type Organizator = {responsib:string}
-// type Speaker = {speechTheme:string}
-// type Developer = {stack:string}
-// type Student = {studiesAt:string}
+type Speaker = {speechTheme:string}
+type Developer = {stack:string}
+type Student = {studiesAt:string}
 
-type Position = 
+type Position =                     // enum
     | Organizator of Organizator
-    // | Speaker of Speaker
-    // | Developer of Developer
-    // | Student of Student 
+    | Speaker of Speaker
+    | Developer of Developer
+    | Student of Student 
 
-type Visitor(id:int, name:string, salary:int, pos:Position) =
-    member this.Position 
+type Visitor(id:int, name:string, pos:Position) = // class
+    member this.PositionField 
         with get() = pos;
 
 
-
-let printToView (pos : Position) = 
+let printToView (pos : Position) =  // function 
     match pos with
     | Organizator pos -> printfn "%s" pos.responsib
-    // | Speaker pos -> printfn "%s" pos.speechTheme
-    // | Developer pos -> printfn "%s" pos.stack
-    // | Student pos -> printfn "%s" pos.studiesAt 
+    | Speaker pos -> printfn "%s" pos.speechTheme
+    | Developer pos -> printfn "%s" pos.stack
+    | Student pos -> printfn "%s" pos.studiesAt 
 
 
 
-let v = Visitor(42, "hello",20000 , Position {responsib = "2helo"})
-//let v:Visitor = Visitor(1, "Dmytro Zhluktenko", Position {responsib = "hello"})
+let v = Visitor(42, "hello" , Speaker {speechTheme = "F# @ .NET"})
 
-
-
-
-
-
-// records
-//type Robusta = {name:string}
-type Arabika = {name:string; customer:string}
-
-type CoffeeTaste = 
-    //| Robusta of Robusta
-    | Arabika of Arabika
-    
-
-type CoffeeMachine(id:int, name:string, price:int, coffee:CoffeeTaste) =
-     member this.Coffee 
-        with get() = coffee
-
-
-
-let printToView (taste : CoffeeTaste) = 
-    match taste with
-    | Arabika taste -> printfn "%s %s" taste.name taste.customer
-    //| Robusta taste -> printfn "%s" taste.name
-    
-
-
-let cm = CoffeeMachine(42, "COFFEEMAKER3000", 200, Arabika { name = "SomeName"; customer = "hello" })
-
-printToView cm.Coffee
-
-
-
+printToView v.PositionField
