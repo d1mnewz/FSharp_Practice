57f591dafe519555519002f733e6d19a4636b273
ZhluktenkoDmytro <d1mnewz@gmail.com>
2017-04-13 22:38:46 +0300

    fix

--- 24_HTML_Parser/check.fsx
+++ 24_HTML_Parser/check.fsx
@@ -8,7 +8,7 @@ let server = "smtp.gmail.com" // ConfigurationManager.AppSettings.["mailserver"]
 let sender = "icanmakeyoucryo.o@gmail.com" // ConfigurationManager.AppSettings.["mailsender"]
 let password = "arizonaboys" // ConfigurationManager.AppSettings.["mailpassword"] |> my-decrypt
 let port = 587
-let SendTest email topic msg =
+let SendTest email topic msg = // doeesnt work
     use msg = 
         new MailMessage(
             sender, email, topic, 
